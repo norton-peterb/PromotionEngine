107ff0880c1a6ce616a3bd61061dae69b548eb2a
Peter B Norton <norton.peterb@gmail.com>
2021-06-30 20:00:18 +0100

    Basic Spring Boot Web Application Boilerplate

---
+++ PromotionEngine/src/main/java/org/engine/promotion/PromotionEngineApplication.java
@@ -0,0 +1,11 @@
+package org.engine.promotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PromotionEngineApplication.class);
+    }
+}
